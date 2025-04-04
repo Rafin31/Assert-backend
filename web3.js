8a712e802183ffcc5a7fb85d9493ba09be229af0
Rafin31 <asifhossain976@gmail.com>
2025-04-04 14:10:47 +1100

    First commit after connecting to sepolia

--- web3.js
+++ web3.js
@@ -7,19 +7,19 @@ const contractABI = JSON.parse(fs.readFileSync("./assets/ATTokenABI.json", "utf-
 dotenv.config(); // Load environment variables
 
 // Connect to Ganache's local blockchain
-const web3 = new Web3(process.env.GANACHE_RPC_URL);
+const web3 = new Web3(process.env.SEPOLIA_RPC_URL);
 
 // Load the wallet account using the private key
-const account = web3.eth.accounts.privateKeyToAccount(process.env.GANACHE_PRIVATE_KEY);
+const account = web3.eth.accounts.privateKeyToAccount('0x' + process.env.METAMASK_PRIVATE_KEY);
 web3.eth.accounts.wallet.add(account);
 
 // Load the deployed ATToken smart contract
-const contract = new web3.eth.Contract(contractABI, process.env.CONTRACT_ADDRESS);
+const contract = new web3.eth.Contract(contractABI, process.env.CONTRACT_ADDRESS_METAMASK);
 
 console.log("---------------------------------------------")
-console.log("Web3.js Connected to Ganache".bold.green.inverse);
+console.log("Web3.js Connected to Sepolia".bold.green.inverse);
 console.log(`Wallet Address: ${account.address}`.bold.green.inverse);
-console.log(`Contract Address: ${process.env.CONTRACT_ADDRESS}`.bold.green.inverse);
+console.log(`Contract Address: ${process.env.CONTRACT_ADDRESS_METAMASK}`.bold.green.inverse);
 
 
 export { web3, contract, account };
