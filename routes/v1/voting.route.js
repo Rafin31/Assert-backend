2bc870fd3f07894d76550601445fd40a4b74ef78
Rashedul Hoque Arif <34288115+rashedulhoque049@users.noreply.github.com>
2025-05-14 00:31:30 +1000

    Updated Dashboard Overview

--- routes/v1/voting.route.js
+++ routes/v1/voting.route.js
@@ -1,9 +1,11 @@
 import express from "express";
 const router = express.Router();
 
-import { castVote, getUserVotes } from "../../controllers/voting.controller.js";
+import { castVote, getUserVotes, getPredictionCount } from "../../controllers/voting.controller.js";
 
 router.route("/vote/user/:userId").get(getUserVotes);
 router.route("/vote/castVote").post(castVote);
+router.route("/getPredictionCount/:userId").get(getPredictionCount);
+
 
 export default router;
